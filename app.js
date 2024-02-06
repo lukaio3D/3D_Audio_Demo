651a4200af6fe3b12f1e2a9c0e64e6fb2fe11612
Lukas Schaab <lukas.schaab@lukaio.de>
2024-02-06 15:45:20 +0100

    updated sound player

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-
-
-//Elemente aus dem DOM holen
-const Vorhang1 = document.getElementById("Vorhang1");
-const Vorhang2 = document.getElementById("Vorhang2");
-const Vorhang3 = document.getElementById("Vorhang3");
-const Vorhang4 = document.getElementById("Vorhang4");
-const Vorhaenge = [Vorhang1, Vorhang2, Vorhang3, Vorhang4];
-const soundBall = document.getElementById("soundBall")
-const keyboard3D = document.getElementById("Keyboard3D")
-const dialogBox = document.getElementById("dialogBox");
-
 //Funktionen
 const VorhangSchließen = (vorhangName) => {
   vorhangName.setAttribute("animation-mixer", {
@@ -27,12 +15,28 @@ function VorhangOeffnen(vorhangName) {
   });
 }
 
+//Elemente aus dem DOM holen
+const Vorhang1 = document.getElementById("Vorhang1");
+const Vorhang2 = document.getElementById("Vorhang2");
+const Vorhang3 = document.getElementById("Vorhang3");
+const Vorhang4 = document.getElementById("Vorhang4");
+const Vorhaenge = [Vorhang1, Vorhang2, Vorhang3, Vorhang4];
+const soundBall = document.getElementById("soundBall");
+const keyboard3D = document.getElementById("Keyboard3D");
+const dialogBox = document.getElementById("dialogBox");
+
+const soundComponent = keyboard3D.getAttribute("sound__birdwhistle3d");
+
 const SchreibeDialog = (dialogtext) => (dialogBox.innerHTML = dialogtext);
 
 const WelcomeUser = () => {
   SchreibeDialog(
     "Hallo Besucher, <br> hinter einem der Vorhänge ist ein Sound versteckt. Können Sie ihn finden?"
   );
+  setTimeout(() => {
+    keyboard3D.components.sound__birdwhistle3d.playSound();
+  }, 3000);
+
   setTimeout(() => {
     SchreibeDialog(
       "Klicken Sie auf den Vorhang hinter dem sich der Sound befindet"
@@ -46,7 +50,7 @@ const VorhangHandler = () => {};
 WelcomeUser();
 
 Vorhang1.onclick = () => {
-  SchreibeDialog("")
+  SchreibeDialog("");
   VorhangOeffnen(Vorhang1);
   setTimeout(() => {
     SchreibeDialog(
@@ -54,13 +58,12 @@ Vorhang1.onclick = () => {
     );
   }, 10000);
   setTimeout(() => {
-    VorhangSchließen(Vorhang1)
+    VorhangSchließen(Vorhang1);
   }, 11000);
-
 };
 
 Vorhang4.onclick = () => {
-  SchreibeDialog("")
+  SchreibeDialog("");
   VorhangOeffnen(Vorhang4);
   setTimeout(() => {
     SchreibeDialog(
@@ -68,13 +71,12 @@ Vorhang4.onclick = () => {
     );
   }, 10000);
   setTimeout(() => {
-    VorhangSchließen(Vorhang4)
+    VorhangSchließen(Vorhang4);
   }, 11000);
-
 };
 
 Vorhang3.onclick = () => {
-  SchreibeDialog("")
+  SchreibeDialog("");
   VorhangOeffnen(Vorhang3);
   setTimeout(() => {
     SchreibeDialog(
@@ -82,21 +84,20 @@ Vorhang3.onclick = () => {
     );
   }, 10000);
   setTimeout(() => {
-    VorhangSchließen(Vorhang3)
+    VorhangSchließen(Vorhang3);
   }, 11000);
-
 };
 
 Vorhang2.onclick = () => {
-  SchreibeDialog("")
+  SchreibeDialog("");
   VorhangOeffnen(Vorhang2);
   setTimeout(() => {
-    SchreibeDialog(
-      "Sie haben den Sound gefunden, Glückwunsch!"
-    );
+    SchreibeDialog("Sie haben den Sound gefunden, Glückwunsch!");
   }, 5000);
   setTimeout(() => {
-    keyboard3D.setAttribute("animation", "property: position; to: -1 0.3 0; dur: 7000;")
+    keyboard3D.setAttribute(
+      "animation",
+      "property: position; to: -1 0.3 0; easing: easeInOutQuad; dur: 7000;"
+    );
   }, 5000);
-
 };
